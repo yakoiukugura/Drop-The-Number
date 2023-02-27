bd8d3f737c85d9fabb605bc9fdbe003917c62d63
Yakoiukugura <56473303+yakoiukugura@users.noreply.github.com>
2023-02-28 01:15:16 +0200

    Add files via upload

---
+++ Drop The Number/Drop The Number/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Drop_The_Number
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.number = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // number
+            // 
+            this.number.AutoSize = true;
+            this.number.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.number.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.number.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number.Location = new System.Drawing.Point(168, 0);
+            this.number.Name = "number";
+            this.number.Padding = new System.Windows.Forms.Padding(5);
+            this.number.Size = new System.Drawing.Size(42, 43);
+            this.number.TabIndex = 0;
+            this.number.Text = "2";
+            this.number.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Best: 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(266, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Score: 0";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(12, 524);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Pause";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.Location = new System.Drawing.Point(138, 524);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button3.Location = new System.Drawing.Point(266, 524);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 576);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.number);
+            this.Name = "Form1";
+            this.Text = "Drop The Number";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label number;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Drop The Number/Drop The Number/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drop_The_Number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int speed = 1, size = 0;
+        Label[] labelArray = new Label[100];
+
+        public static Label CopyLabel(Label label)
+        {
+            Label l = new Label();
+            l.Font = label.Font;
+            l.Width = label.Width;
+            l.Height = label.Height;
+            l.Margin = label.Margin;
+            l.Text = "2";
+            l.TextAlign = label.TextAlign;
+            l.BorderStyle = label.BorderStyle;
+            l.BackColor = label.BackColor;
+            l.ForeColor = label.ForeColor;
+            l.Visible = true;
+            return l;
+        }
+
+        private int check_Collision()
+        {
+            int bottom = number.Top + number.Height;
+            for (int i = 0; i < size; i++)
+                if (number.Bounds.IntersectsWith(labelArray[i].Bounds))
+                    return 2;
+            if (bottom >= 507)
+                return 1;
+            return 0;
+        }
+
+        private void merge(int i)
+        {
+            labelArray[i].Text = (Convert.ToInt32(labelArray[i].Text) * 2).ToString();
+            number.Location = new Point(168, 0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            number.Top += speed;
+            if (check_Collision() == 1)
+            {
+                labelArray[size++] = number;
+                number = CopyLabel(number);
+                number.Location = new Point(168, 0);
+                this.Controls.Add(number);
+            }
+            if (check_Collision() == 2)
+            {
+                int i;
+                for (i = 0; i < size; i++)
+                    if (number.Bounds.IntersectsWith(labelArray[i].Bounds))
+                        break;
+                if (number.Text == labelArray[i].Text)
+                    merge(i);
+                else
+                {
+                    labelArray[size++] = number;
+                    number = CopyLabel(number);
+                    number.Location = new Point(168, 0);
+                    this.Controls.Add(number);
+                }
+            }
+            for (int i = 0; i < size - 1; i++)
+                for (int j = i + 1; j < size; j++)
+                    if (labelArray[i].Bounds.IntersectsWith(labelArray[j].Bounds) && labelArray[i].Text == labelArray[j].Text)
+                    { 
+                        merge(i);
+                        labelArray[j].Visible = false;
+                        labelArray[j].Enabled = false;
+                        for (int p = j; p < size - 1; p++)
+                            labelArray[p] = labelArray[p + 1];
+                        size--;
+                    }
+            for (int i = 0; i < size; i++)
+            {
+                if (labelArray[i].Text == "4")
+                    labelArray[i].BackColor = Color.Green;
+                else if (labelArray[i].Text == "8")
+                    labelArray[i].BackColor = Color.Blue;
+                else if (labelArray[i].Text == "16")
+                    labelArray[i].BackColor = Color.Red;
+                else if (labelArray[i].Text == "32")
+                    labelArray[i].BackColor = Color.Purple;
+                else if (labelArray[i].Text == "64")
+                    labelArray[i].BackColor = Color.Gold;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                number.Left -= number.Width;
+                if (number.Left < 0)
+                    number.Left += number.Width;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                number.Left += number.Width;
+                if (number.Left + number.Width > 394)
+                    number.Left -= number.Width;
+            }
+        }
+    }
+}
